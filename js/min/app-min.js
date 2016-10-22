fd3df2896cb6cf03cb4e05ae66f1aec0980886ac
Antoine Catacutan <antoinecatt11@Antoines-MacBook-Pro.local>
2016-10-22 11:05:02 -0700

    First commit

---
+++ js/min/app-min.js
@@ -0,0 +1,9 @@
+$(document).foundation();
+//# sourceMappingURL=./app-min.js.map
+
+// $(window).resize(function() {
+//   var viewport = $(this).width();
+//   if(viewport < 640) {
+//     $(".toggle").toggleClass(".top-bar-right");
+//   }
+// });
